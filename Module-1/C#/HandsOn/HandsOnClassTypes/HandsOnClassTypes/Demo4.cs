45d376472248e5de06da952c03017cd45004f100
santuparsi <santuparsi@gmail.com>
2021-04-20 12:49:03 +0530

    initial

---
+++ Module-1/C#/HandsOn/HandsOnClassTypes/HandsOnClassTypes/Demo4.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsOnClassTypes
+{
+    //Abstract class 
+    abstract class Shape
+    {
+        public abstract void Area(); //abstract function
+        public void Type() //not abstract function/method
+        {
+            Console.WriteLine("I am a 2D Shape.");
+        }
+    }
+    class Circle1:Shape
+    {
+        double r;
+        public Circle1(double r)
+        {
+            this.r = r;
+        }
+        public override void Area()
+        {
+            Console.WriteLine("Area of Circle: " + Math.PI * r * r);
+        }
+    }
+    class Rectangle:Shape
+    {
+        int l, w;
+        public Rectangle(int l,int w)
+        {
+            this.l = l;
+            this.w = w;
+        }
+        public override void Area()
+        {
+            Console.WriteLine("Area of Rectnalge: " + (l * w));
+        }
+    }
+    class Demo4
+    {
+        static void Main()
+        {
+            Shape circle = new Circle1(12.3);
+            Shape rectangle = new Rectangle(2, 4);
+            circle.Area();
+            rectangle.Area();
+            Shape[] shapes = new Shape[2];
+            shapes[0] = circle;
+            shapes[1] = rectangle;
+            foreach (Shape s in shapes)
+            {
+                s.Area();
+                s.Type();
+            }
+        }
+    }
+}
---
+++ Module-1/C#/HandsOn/HandsOnClassTypes/HandsOnClassTypes/Demo6.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsOnClassTypes
+{
+    class Round<T,K>
+    {
+        public T i;
+        public K j;
+    }
+    class Demo6
+    {
+        static void Main()
+        {
+            Round<int, double> round = new Round<int, double>();
+            round.i = 10;
+            round.j = 2.34;
+            Round<string, int> round1 = new Round<string, int>();
+        }
+    }
+}
