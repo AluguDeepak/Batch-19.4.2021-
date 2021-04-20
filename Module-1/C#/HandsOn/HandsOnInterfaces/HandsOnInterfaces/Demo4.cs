80b4d6d1dc5de9f66230127ab6f5c3986b9000e6
santuparsi <santuparsi@gmail.com>
2021-04-20 16:14:46 +0530

    initial

---
+++ Module-1/C#/HandsOn/HandsOnInterfaces/HandsOnInterfaces/Demo4.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsOnInterfaces
+{
+    //Generic Interfaces
+   public interface ISample<T>
+    {
+        T M();
+        void M1(T a);
+    }
+
+    class Demo4 : ISample<int>
+    {
+        public int M()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void M1(int a)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    class Demo5 : ISample<string>
+    {
+        public string M()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void M1(string a)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Module-1/C#/HandsOn/HandsOnNullableTypes/HandsOnNullableTypes/Product.cs
+++ Module-1/C#/HandsOn/HandsOnNullableTypes/HandsOnNullableTypes/Product.cs
@@ -10,7 +10,7 @@ namespace HandsOnNullableTypes
     {
         public int Pid { get; set; }
         public string Pname { get; set; }
-        public double Price { get; set; }
-        public int Stock { get; set; }
+        public double? Price { get; set; }
+        public int? Stock { get; set; }
     }
 }
