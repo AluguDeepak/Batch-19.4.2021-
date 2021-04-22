a9768f651222ccb7ca50eaea6abf1b8edcc3fb46
santuparsi <santuparsi@gmail.com>
2021-04-22 17:40:07 +0530

    initial

---
+++ Module-1/C#/HandsOn/HandsOnCollections/HandsOnCollections/HashTableDemo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+namespace HandsOnCollections
+{
+    class HashTableDemo
+    {
+        static void Main()
+        {
+            Hashtable hs = new Hashtable();
+            hs.Add(1000, "Rohan");
+            hs.Add('a', "Apple");
+            hs.Add(1002, "Karan");
+            hs.Add(1003, "Suresh");
+            //access a value
+            string s = hs[1000].ToString();
+            foreach(object k in hs.Keys)
+            {
+                Console.WriteLine($"Key:{k} Value:{hs[k]}");
+            }
+            foreach(DictionaryEntry d in hs)
+            {
+                Console.WriteLine("{0} {1}", d.Key, d.Value);
+            }
+
+            hs.Remove('a'); //pass key value to remove the key-value pair.
+        }
+    }
+}
---
+++ Module-1/C#/HandsOn/HandsOnCollections/HandsOnCollections/HashTableDemo2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+namespace HandsOnCollections
+{
+    class HashTableDemo2
+    {
+        static void Main()
+        {
+            Product p = new Product() { Pid = 1, Pname = "Keyboard", Price = 100, Stock = 100 };
+            Product p2 = new Product() { Pid = 2, Pname = "Mouse", Price = 400, Stock = 100 };
+            Product p1= new Product() { Pid = 3, Pname = "KeyPad", Price = 100, Stock = 100 };
+            Hashtable hs = new Hashtable();
+            hs.Add(p.Pid, p);
+            hs.Add(p1.Pid, p1);
+            hs.Add(p2.Pid, p2);
+            Product product = hs[1] as Product;
+            hs.Remove(1);
+            foreach(DictionaryEntry d in hs)
+            {
+                Product pp = d.Value as Product;
+                Console.WriteLine("Product {0} Details {1} {2} {3}", d.Key, pp.Pname, pp.Price, pp.Stock);
+            }
+        }
+    }
+}
