24df0ae4f365e333dd3a41b0d7b2a28517cd9e2a
santuparsi <santuparsi@gmail.com>
2021-06-08 12:41:54 +0530

    68

---
+++ Module-3/EF/HandsOnEFCore/HandsOnEFCore-DataBaseFirst/GenericRepository/GenericRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HandsOnEFCore_DataBaseFirst.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HandsOnEFCore_DataBaseFirst.GenericRepository
+{
+    class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private Batch2DBContext _context = null;
+        private DbSet<T> table = null;
+        public GenericRepository()
+        {
+            _context = new Batch2DBContext();
+            table = _context.Set<T>();
+        }
+        public void Delete(object id)
+        {
+            T obj = table.Find(id);
+            table.Remove(obj);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return table.ToList();
+        }
+
+        public T GetById(object id)
+        {
+            return table.Find(id);
+        }
+
+        public void Insert(T obj)
+        {
+            table.Add(obj);
+            Save();
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Update(T obj)
+        {
+            table.Update(obj);
+            Save();
+        }
+    }
+}
--- Module-3/EF/HandsOnEFCore/HandsOnEFCore-DataBaseFirst/Program.cs
+++ Module-3/EF/HandsOnEFCore/HandsOnEFCore-DataBaseFirst/Program.cs
@@ -1,16 +1,28 @@
 ﻿using System;
 using HandsOnEFCore_DataBaseFirst.Entities;
 using HandsOnEFCore_DataBaseFirst.Repositories;
+using HandsOnEFCore_DataBaseFirst.GenericRepository;
 namespace HandsOnEFCore_DataBaseFirst
 {
     class Program
     {
         static void Main(string[] args)
         {
-            do
+            //do
+            //{
+            //    Menu();
+            //} while (true);
+            // GenericRepository<Product> repository = new GenericRepository<Product>();
+            //foreach (var item in repository.GetAll())
+            //{
+            //    Console.WriteLine("{0} {1} {2} {3}", item.Pid, item.Pname, item.Price, item.Stock);
+            //}
+            GenericRepository<Employee> repository = new GenericRepository<Employee>();
+            foreach(var item in repository.GetAll())
             {
-                Menu();
-            } while (true);
+                Console.WriteLine("{0} {1}",item.Eid,item.Ename);
+            }
+           
 
         }
 
@@ -46,6 +58,30 @@ namespace HandsOnEFCore_DataBaseFirst
                             {
                                 Console.WriteLine("{0} {1} {2} {3}",item.Pid,item.Pname,item.Price,item.Stock);
                             }
+                        }
+                        break;
+                    case 3:
+                        {
+                            var item = repository.GetProduct(100);
+                            Console.WriteLine("{0} {1} {2} {3}", item.Pid, item.Pname, item.Price, item.Stock);
+                        }
+                        break;
+                    case 4:
+                        {
+                            repository.DeleteProduct(100);
+                        }
+                        break;
+                    case 5:
+                        {
+                            var item= repository.GetProduct(100);
+                            item.Price = 450;
+                            item.Stock = 100;
+                            repository.UpdateProduct(item);
+                        }
+                        break;
+                    case 6:
+                        {
+                            Environment.Exit(0);
                         }
                         break;
                 }
