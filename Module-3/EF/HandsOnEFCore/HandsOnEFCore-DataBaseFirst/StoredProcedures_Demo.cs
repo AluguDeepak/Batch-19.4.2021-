d5c3c0036c6a0041fa0fa3b73059583eac034f00
santuparsi <santuparsi@gmail.com>
2021-06-08 14:24:54 +0530

    68

---
+++ Module-3/EF/HandsOnEFCore/HandsOnEFCore-DataBaseFirst/StoredProcedures_Demo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HandsOnEFCore_DataBaseFirst.Entities;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+namespace HandsOnEFCore_DataBaseFirst
+{
+    class StoredProcedures_Demo
+    {
+
+        static void Main()
+        {
+            Batch2DBContext dBContext = new Batch2DBContext();
+            List<Product> products = dBContext.Products.FromSqlRaw("GetProducts").ToList();
+            //foreach(var item in products)
+            //{
+            //    Console.WriteLine("{0} {1}",item.Pid,item.Pname);
+            //}
+            var product = dBContext.Products.FromSqlRaw("GetProductById @Pid", new SqlParameter("@Pid", 1));
+            foreach (var item in product)
+            {
+                Console.WriteLine("{0} {1}", item.Pid, item.Pname);
+            }
+            //Add Product
+            dBContext.Database.ExecuteSqlRaw("AddProduct @p0,@p1,@p2,@p3",
+                parameters: new object[] { 111, "CCCC", 340, 120 });
+        }
+    }
+}
