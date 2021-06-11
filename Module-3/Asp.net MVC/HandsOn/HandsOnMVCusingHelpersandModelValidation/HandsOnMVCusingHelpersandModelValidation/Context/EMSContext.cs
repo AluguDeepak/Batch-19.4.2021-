52e494aaf83eb97698519c4b156909eb6897bf98
santuparsi <santuparsi@gmail.com>
2021-06-11 13:07:02 +0530

    611_2

---
+++ Module-3/Asp.net MVC/HandsOn/HandsOnMVCusingHelpersandModelValidation/HandsOnMVCusingHelpersandModelValidation/Context/EMSContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HandsOnMVCusingHelpersandModelValidation.Models;
+namespace HandsOnMVCusingHelpersandModelValidation.Context
+{
+    public class EMSContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=SANTU\MSSQLSERVER2019;Initial Catalog=EMSDB1;User ID=sa;Password=pass@word1");
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
--- Module-3/Asp.net MVC/HandsOn/HandsOnMVCusingHelpersandModelValidation/HandsOnMVCusingHelpersandModelValidation/Controllers/EmployeeController.cs
+++ Module-3/Asp.net MVC/HandsOn/HandsOnMVCusingHelpersandModelValidation/HandsOnMVCusingHelpersandModelValidation/Controllers/EmployeeController.cs
@@ -1,20 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HandsOnMVCusingHelpersandModelValidation.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using HandsOnMVCusingHelpersandModelValidation.Context;
 namespace HandsOnMVCusingHelpersandModelValidation.Controllers
 {
     public class EmployeeController : Controller
     {
+        EMSContext db = null;
+        public EmployeeController()
+        {
+            db = new EMSContext();
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Employee> employees = db.Employees.ToList();
+            return View(employees);
         }
         public IActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                //write code to add Employee details to table
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
         }
         public IActionResult Create1() //design the template using stronly typed helpers
         {
@@ -23,6 +43,21 @@ namespace HandsOnMVCusingHelpersandModelValidation.Controllers
         public IActionResult Create2() //design the template using tag helpers
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create2(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                //write code to add Employee details to table
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
